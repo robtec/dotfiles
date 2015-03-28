2ecee9053404e95fd23d9a478ff6fb19f0cf7d90
Tom Barlow <tomwbarlow@gmail.com>
2015-03-28 00:05:27 +0000

    Added docker helper functions

---
+++ config/fish/config.fish
@@ -0,0 +1,16 @@
+function devbox
+    docker run -it \
+        -v /Users/tbarlow/Dropbox/keys/ssh_tombee:/home/dev/.ssh \
+        -v /Users/tbarlow/dev/devbox-$argv[1]:/home/dev \
+        --name $argv[1] \
+        --hostname $argv[1] \
+        tombee/devbox
+end
+
+function clean_old_docker_containers
+    docker ps -a | grep Exit | awk '{ print $1; }' | xargs docker rm
+end
+
+function clean_docker_images
+    docker images | grep "<none>" | awk '{ print $3;  }' | xargs docker rmi
+end
